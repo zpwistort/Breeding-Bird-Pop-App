5b877464574625392b4669ab1a2469346c865956
Zack <zpwistort@gmail.com>
2021-05-16 10:34:41 -0600

    update to global code

--- global.R
+++ global.R
@@ -9,45 +9,10 @@ options(shiny.usecairo=T)
 
 ## data
 
-# CA_data <- read.csv('CA_occurrence_data.csv')
-# US_data1 <- read.csv('US_occurence_data1.csv')
-# US_data2 <- read.csv('US_occurence_data1.csv')
-# data <- bind_rows(CA_data,US_data1,US_data2)
 
-data2 <- read.csv('data_filtered_2000.csv')
+data2 <- bind_rows(read.csv('data1.csv'),read.csv('data2.csv'),read.csv('data3.csv'))
 routes <- read.csv('routes.csv')
 species <- read.csv('species.csv')
-
-
-# data_filtered_2000
-# data_join <-
-# data %>%
-#   # filter(Year >= 2000) %>%
-#   left_join(routes, by=c('CountryNum','StateNum','Route')) %>%
-#   mutate(lab = paste0(RouteName,': ', as.character(SpeciesTotal)))
-
-
-# write.csv(data_filtered_2000, 'data_filtered_2000.csv')
-
-
-# 
-# 
-# colnames(data_join)
-# 
-# 
-# data_filtered_2000 <-
-#   data_join %>% 
-#     select(-c('Active','BCR','Stratum','RouteTypeDetailID'))
-# 
-# 
-# data_filtered_2000 %>% View()
-# 
-# 
-# write.csv(data_filtered_2000, 'data_filtered_2000.csv')
-
-# min <- range(data2$Year)[1]
-# max <- range(data2$Year)[2]
-
 
 
 
